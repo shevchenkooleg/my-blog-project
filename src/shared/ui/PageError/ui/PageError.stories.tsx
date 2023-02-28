4fc8d20a8db48dc2ab74545a777a1ec3a52e7f7a
Oleg <shevchenko.oleg.87@gmail.com>
2023-02-28 12:31:23 +0300

    add Loader and PageError loki tests

---
+++ src/shared/ui/PageError/ui/PageError.stories.tsx
@@ -0,0 +1,22 @@
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/Decorators/ThemeDecorator';
+import { Theme } from "app/providers/ThemeProvider";
+import { PageError } from "shared/ui/PageError";
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export default {
+    title: 'shared/PageError',
+    component: PageError,
+    argTypes: {
+        backgroundColor: { control: 'color' }
+    }
+} as ComponentMeta<typeof PageError>;
+
+const Template: ComponentStory<typeof PageError> = (args) => <PageError/>;
+
+export const Primary = Template.bind({});
+Primary.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)]
