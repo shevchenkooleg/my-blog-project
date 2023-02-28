bf84c75b0904fc2623b184511667c0a6e5fcb6fd
Oleg <shevchenko.oleg.87@gmail.com>
2023-02-28 10:52:53 +0300

    story experement

---
+++ src/shared/config/storybook/Decorators/ThemeDecorator.tsx
@@ -0,0 +1,11 @@
+import { type Story } from "@storybook/react";
+import { type Theme } from "app/providers/ThemeProvider";
+
+
+// eslint-disable-next-line react/display-name
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => {
+    return (
+        <div className={`app ${theme}`}>
+            <StoryComponent/>
+        </div>)
+}
