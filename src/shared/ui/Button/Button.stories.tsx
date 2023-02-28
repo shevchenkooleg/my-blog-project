733a02f011cd44d81de4eac5a840b1a0328912f1
Oleg <shevchenko.oleg.87@gmail.com>
2023-02-28 11:51:23 +0300

    add outline story

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { type ComponentStory, type ComponentMeta } from '@storybook/react';
 import { Button, ThemeButton } from './Button';
 import '../../../app/styles/index.scss'
+import { ThemeDecorator } from "shared/config/storybook/Decorators/ThemeDecorator";
+import { Theme } from "app/providers/ThemeProvider";
+import { Light } from "pages/AboutPage/ui/AboutPage.stories";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
@@ -17,21 +20,43 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+export const Primary_Light = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
+Primary_Light.args = {
     children: 'Text'
 };
+Primary_Light.decorators = [ThemeDecorator(Theme.LIGHT)]
 
+export const Primary_Dark = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary_Dark.args = {
+    children: 'Text'
+};
+Primary_Dark.decorators = [ThemeDecorator(Theme.DARK)]
 
-export const Clear = Template.bind({});
-Clear.args = {
+export const Clear_Light = Template.bind({});
+Clear_Light.args = {
     children: 'Text',
     theme: ThemeButton.CLEAR
 };
+Clear_Light.decorators = [ThemeDecorator(Theme.LIGHT)]
+export const Clear_Dark = Template.bind({});
+Clear_Dark.args = {
+    children: 'Text',
+    theme: ThemeButton.CLEAR
+};
+Clear_Dark.decorators = [ThemeDecorator(Theme.DARK)]
 
-export const Outline = Template.bind({});
-Outline.args = {
+export const Outline_Light = Template.bind({});
+Outline_Light.args = {
     children: 'Text',
     theme: ThemeButton.OUTLINE
 };
+Outline_Light.decorators = [ThemeDecorator(Theme.LIGHT)]
+
+export const Outline_Dark = Template.bind({});
+Outline_Dark.args = {
+    children: 'Text',
+    theme: ThemeButton.OUTLINE
+};
+Outline_Dark.decorators = [ThemeDecorator(Theme.DARK)]
