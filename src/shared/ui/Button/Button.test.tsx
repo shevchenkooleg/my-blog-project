1e30db047f335810f924250c46f03659ad1bc57c
Oleg <shevchenko.oleg.87@gmail.com>
2023-03-01 08:52:12 +0300

    fix(switch off) linting errors in testing files

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -4,10 +4,12 @@ import { render, screen } from "@testing-library/react";
 
 describe('Button tests', () => {
     test('button will be render in the document', () => {
+        // eslint-disable-next-line i18next/no-literal-string
         render(<Button>TEXT</Button>)
         expect(screen.getByText('TEXT')).toBeInTheDocument()
     })
     test('button will be render with clear theme', () => {
+        // eslint-disable-next-line i18next/no-literal-string
         render(<Button theme={ThemeButton.CLEAR}>TEXT</Button>)
         expect(screen.getByText('TEXT')).toHaveClass('clear')
         screen.debug()
