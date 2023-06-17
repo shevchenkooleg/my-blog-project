10d5ca6b2d255db825e2c19697cc86eb47b9f20a
Oleg <shevchenko.oleg.87@gmail.com>
2023-06-17 15:13:26 +0300

    fix types error in cypress test-case

--- cypress/component/EditableProfileCard.cy.tsx
+++ cypress/component/EditableProfileCard.cy.tsx
@@ -1,14 +1,16 @@
-import { describe, it, cy } from 'local-cypress'
-import { EditableProfileCard } from "@/features/editableProfileCard";
-import { TestProvider } from "@/shared/lib/test/componentRender/componentRender";
+import { describe, it, cy } from 'local-cypress';
+import { EditableProfileCard } from '@/features/editableProfileCard';
+import { TestProvider } from '@/shared/lib/test/componentRender/componentRender';
 
 describe('EditableProfileCard.cy.tsx', () => {
     it('playground', () => {
-        cy.intercept('GET', '**/profile/*', { fixture: 'profile.json' })
+        cy.intercept('GET', '**/profile/*', { fixture: 'profile.json' });
         cy.mount(
-            <TestProvider options={{ initialState: { user: { authData: { id: '4' } } } }}>
-                <EditableProfileCard id={'4'}/>
+            <TestProvider
+                options={{ initialState: { user: { authData: { id: 4 } } } }}
+            >
+                <EditableProfileCard id={'4'} />
             </TestProvider>
-        )
-    })
-})
+        );
+    });
+});
